57ebd7f7583b4d054e0e60a96f4b0d2f13ba41b3
RachCiar <38289233+RachCiar@users.noreply.github.com>
2022-10-22 17:35:18 -0400

    fifth module update

--- api/src/router.js
+++ api/src/router.js
@@ -2,12 +2,13 @@ const {Router} = require('express');
 const { request } = require('http');
 const multer = require('multer');
 const path = require('path');
+const imageProcessor = require('./imageProcessor');
 
 const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
 
 const router = Router();
 
-let filename = (request, file, callback)=>{
+const filename = (request, file, callback)=>{
 callback(null, file.originalname);
 };
 
@@ -30,10 +31,14 @@ const upload = multer({
     storage
 });
 
-router.post('/upload', upload.single('photo'),(request, response)=>{
-    if(request.fileValidationError) return response.status(400).json({
-        error: request.fileValidationError
-    });
+router.post('/upload', upload.single('photo'), async (request, response)=>{
+    if(request.fileValidationError) return response.status(400).json({error: request.fileValidationError});
+
+    try{
+        await imageProcessor(request.file.filename);
+    }catch(error){
+
+    }
     return response.status(201).json({success: true});
 });
 
